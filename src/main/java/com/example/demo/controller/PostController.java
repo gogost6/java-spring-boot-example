1cb400a98cbfc86fd33c84b6ab80efd228b8d5f3
gogost6 <gogost6@gmail.com>
2026-05-03 16:55:53 +0300

    fix: unused imports

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entity.Post;
-import com.example.demo.repository.PostRepository;
-import com.example.demo.service.PostServiceExternal;
 
 @RestController
 @RequestMapping("/api/posts")
